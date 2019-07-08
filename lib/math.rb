ee657329f11ec583759538567855851733c3ab3d
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-08 22:25:35 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -6,21 +6,26 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  puts answer
+  return answer
 end
 
 def division(num1, num2)
-
+  puts answer
+  return answer
 end
 
 def multiplication(num1, num2)
-
+  puts answer
+  return answer
 end
 
 def modulo(num1, num2)
-
+  puts answer
+  return answer
 end
 
 def square_root(num)
-
+  puts answer
+  return answer
 end
